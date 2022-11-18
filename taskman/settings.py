2f0965e91717c6ee33be83b24ac9360030399ba8
Денис Лошкарёв <45289831+qutoq@users.noreply.github.com>
2022-11-18 23:02:39 +0300

    Update settings.py

--- taskman/settings.py
+++ taskman/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p2pj@2bnxs0mq(vg*4^*9nohl0a@9hokfab)3pcopm2pf#fbgx'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
